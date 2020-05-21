9007408207394d39cd6b9213c36d23d9a175f876
IanGHill <ian.g.hill@outlook.com>
2020-05-21 20:25:56 +0100

    First commit

---
+++ src/components/ControlComponent.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class ControlComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    }
+
+  }
+
+
+  render() {
+    return (
+        <h1>I am a control component</h1>
+    );
+  }
+}
+
+export default ControlComponent;
---
+++ src/components/ImageComponent.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class ImageComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    }
+
+  }
+
+
+  render() {
+    return (
+        <h1>I am an image</h1>
+    );
+  }
+}
+
+export default ImageComponent;
